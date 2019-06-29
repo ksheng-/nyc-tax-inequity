d87885022fae37be6d383a951b7b64c38220f191
ksheng- <kevin.s.sheng@gmail.com>
2019-06-28 20:01:58 -0400

    remove nyc-db

--- nyc-db/src/nycdb/sql/dobjobs/add_columns.sql
+++
@@ -1,8 +0,0 @@
-alter table dobjobs add column address text;
-alter table dobjobs add column ownername text;
-alter table dobjobs add column applicantname text;
-
-update dobjobs
-       set address = house || ' ' || streetname || ', ' || zip,
-       ownername = ownersfirstname || ' ' || ownerslastname,
-       applicantname = applicantsfirstname || ' ' || applicantslastname;
